17478b2d6960b06f49a25a9a2425f9052f00bdd3
Alexander Wagner <awagner@bib.uni-mannheim.de>
2015-02-02 13:54:15 +0000

    Initial Upload UB Mannheim App iOS Swift Version

---
+++ UBMannheimApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UBMannheimApp
+//
+//  Created by Alexander Wagner on 27.01.15.
+//  Copyright (c) 2015 Alexander Wagner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
