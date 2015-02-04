d4c166fe5d712ae7e0940100b13d5c2873e034a2
Alexander Wagner <awagner@bib.uni-mannheim.de>
2015-02-04 15:27:31 +0000

    Added Custom Navigation Bar to AppDelegate Created Function to convert HexColor to UIColor
    Signed-off <alexander.wagner@bib.uni-mannheim.de>

--- UBMannheimApp/AppDelegate.swift
+++ UBMannheimApp/AppDelegate.swift
@@ -13,9 +13,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func uicolorFromHex(rgbValue:UInt32)->UIColor{
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16)/256.0
+        let green = CGFloat((rgbValue & 0xFF00) >> 8)/256.0
+        let blue = CGFloat(rgbValue & 0xFF)/256.0
+        
+        return UIColor(red:red, green:green, blue:blue, alpha:1.0)
+    }
+    
+    
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        var navigationBarAppearace = UINavigationBar.appearance()
+        
+        navigationBarAppearace.tintColor = uicolorFromHex(0xffffff)
+        navigationBarAppearace.barTintColor = uicolorFromHex(0x990000)
+        
+        navigationBarAppearace.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+
+        
         return true
     }
 
