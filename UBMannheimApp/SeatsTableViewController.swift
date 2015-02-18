2ccbc12ceeaf0eed871c1fec7e41f63a7f1afc9c
Alexander Wagner <awagner@bib.uni-mannheim.de>
2015-02-18 11:13:23 +0000

    Added Documentation and implemented Module FreeSeats - finally works :) -  basend on JSON
    Signed-off-by: Alexander Wagner <alexander.wagner@bib.uni-mannheim.de>

--- UBMannheimApp/SeatsTableViewController.swift
+++ UBMannheimApp/SeatsTableViewController.swift
@@ -13,13 +13,82 @@ import Foundation
 
 class SeatsTableViewController: UITableViewController {
 
-    var items: AnyObject = []
-    // var url: NSURL = NSURL()
-
+    var items: NSArray = NSArray()
     var data: NSMutableData = NSMutableData()
     
-    func searchItunesFor(searchTerm: String) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        // searchItunesFor("JQ Software")
+        loadJSONFromURL()
+        
+        // Cell height.
+        self.tableView.rowHeight = 70
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        // self.tableView.reloadData()
+        
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "cell")
+        
+        var rowData: NSDictionary = self.items[indexPath.row] as NSDictionary
+        
+        // cell.textLabel?.text = rowData["trackName"] as String!
+        cell.textLabel?.text = rowData["id"] as String!
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        // var urlString: NSString = rowData["artworkUrl60"] as NSString
+        // var imgURL: NSURL = NSURL(string: urlString)!
+        
+        // Download an NSData representation of the image at the URL
+        // var imgData: NSData = NSData(contentsOfURL: imgURL)!
+        // cell.imageView?.image = UIImage(data: imgData)
+        
+        // Get the formatted price string for display in the subtitle
+        // var formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        var loadInPercent: Int = rowData["percent"] as Int
+        var maxCountOfSeats: Int = rowData["max"] as Int
+        
+        
+        if(loadInPercent >= 81) {
+                cell.imageView?.image = UIImage(named: "sign_red.png")
+        }
+        if(loadInPercent < 81) {
+            cell.imageView?.image = UIImage(named: "sign_yellow.png")
+        }
+        if(loadInPercent < 61) {
+            cell.imageView?.image = UIImage(named: "sign_green.png")
+        }
+        
+        cell.detailTextLabel?.text = loadInPercent.description + "% (von " + maxCountOfSeats.description + " Arbeitsplätzen sind belegt)"
+        
+        return cell
+    }
+
+    func loadJSONFromURL() {
+        
+        var urlPath = "http://www.bib.uni-mannheim.de/bereichsauslastung/index.php?json"
+        var url: NSURL = NSURL(string: urlPath)!
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
+        
+        println("Search UB JSON Bereichsauslastung at URL \(url)")
+        
+        connection.start()
+    }
+    
+    /*
+    func searchItunesFor(searchTerm: String) {
         // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
         var itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
         
@@ -34,6 +103,7 @@ class SeatsTableViewController: UITableViewController {
         
         connection.start()
     }
+    */
     
     func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
         // Recieved a new request, clear out the data object
@@ -52,266 +122,12 @@ class SeatsTableViewController: UITableViewController {
         var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
         
         if jsonResult.count>0 /*&& jsonResult["results"].count>0*/ {
-            var results: NSArray = jsonResult["results"] as NSArray
+            var results: NSArray = jsonResult["sections"] as NSArray
             self.items = results
             self.tableView.reloadData()
             
         }
     }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items.count
-    }
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "cell")
-        
-        var rowData: NSDictionary = self.items[indexPath.row] as NSDictionary
-        
-        cell.textLabel?.text = rowData["trackName"] as String
-        
-        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
-        var urlString: NSString = rowData["artworkUrl60"] as NSString
-        var imgURL: NSURL = NSURL(string: urlString)!
-        
-        // Download an NSData representation of the image at the URL
-        var imgData: NSData = NSData(contentsOfURL: imgURL)!
-        cell.imageView?.image = UIImage(data: imgData)
-        
-        // Get the formatted price string for display in the subtitle
-        var formattedPrice: NSString = rowData["formattedPrice"] as NSString
-        
-        cell.detailTextLabel?.text = formattedPrice
-        
-        return cell
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        searchItunesFor("JQ Software")
-        
-        
-        
-        // let json = JSON(data: dataFromNetworking)
-        // http://www.bib.uni-mannheim.de/bereichsauslastung/index.php?json
-        
-        
-        /*HTTPGetJSON("http://www.bib.uni-mannheim.de/bereichsauslastung/index.php?json") {
-            (data: Dictionary<String, AnyObject>, error: String?) -> Void in
-            
-            if (error != nil) {
-                println(error)
-            } else {
-
-
-                if let feed = data["date"] as? NSString {
-                    println(feed)
-                }
-                
-                //if let feed = data["sections"] as? NSDictionary {
-                //    for f in feed {
-                //        println(f)
-                //    }
-                //}
-                
-                
-                // http://owensd.io/2014/06/18/json-parsing.html
-                
-                println(".")
-                
-                let dict = data as Dictionary<String, AnyObject>
-                let sections : AnyObject? = dict["sections"]?["section"]
-                let collection = sections! as Array<Dictionary<String, AnyObject>>
-                for section in collection {
-                    let id : AnyObject? = section["id"]
-                    let name : AnyObject? = section["name"]
-                    let percent : AnyObject? = section["percent"]
-                    let max : AnyObject? = section["max"]
-                    
-                    /*
-                    println("Section ID: \(id)")
-                    println("Section Name: \(name)")
-                    println("Section Seats \(percent)")
-                    println("Section Max: \(max)")
-                    */
-                    
-                    var n: NSString = name as NSString!
-                    println(n)
-                    
-                    self.items.append("C")
-                
-                }
-   
-            } // else
-  
-        }*/
-        
-        // println("something")
-        
-        // Cell height.
-        self.tableView.rowHeight = 70
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
-        
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
-        self.tableView.reloadData()
-        
-    }
-
-/*
-
-    // func loadJSON(data: NSURL) {
-    func loadJSON() {
-        
-        let urlAsString = "http://www.bib.uni-mannheim.de/bereichsauslastung/index.php?json"
-        let url = "http://gdata.youtube.com/feeds/api/standardfeeds/most_popular?v=2&alt=json"
-        
-        tableView.reloadData()
-    }
-
-     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-    // MARK: - Table view data source
-     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items.count
-    }
-    
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
-        
-        cell.textLabel?.text = self.items[indexPath.row] as String
-        // cell.detailTextLabel?.text = self.items[indexPath.row] as? String
-        return cell
-
-    }
-    
-    /// /// /// /// 
-    
-    func HTTPsendRequest(request: NSMutableURLRequest,
-        callback: (String, String?) -> Void) {
-            let task = NSURLSession.sharedSession().dataTaskWithRequest(
-                request,
-                {
-                    data, response, error in
-                    if error != nil {
-                        callback("", error.localizedDescription)
-                    } else {
-                        callback(
-                            NSString(data: data, encoding: NSUTF8StringEncoding)!,
-                            nil
-                        )
-                    }
-            })
-            
-            task.resume()
-    }
-    func HTTPGet(url: String, callback: (String, String?) -> Void) {
-        var request = NSMutableURLRequest(URL: NSURL(string: url)!)
-        HTTPsendRequest(request, callback)
-    }
-    func JSONParseDict(jsonString:String) -> Dictionary<String, AnyObject> {
-        var e: NSError?
-        var data: NSData = jsonString.dataUsingEncoding(
-            NSUTF8StringEncoding)!
-        var jsonObj = NSJSONSerialization.JSONObjectWithData(
-            data,
-            options: NSJSONReadingOptions(0),
-            error: &e) as Dictionary<String, AnyObject>
-        if( e != nil) {
-            return Dictionary<String, AnyObject>()
-        } else {
-            return jsonObj
-        }
-    }
-    func HTTPGetJSON(
-        url: String,
-        callback: (Dictionary<String, AnyObject>, String?) -> Void) {
-            var request = NSMutableURLRequest(URL: NSURL(string: url)!)
-            request.setValue("application/json", forHTTPHeaderField: "Accept")
-            HTTPsendRequest(request) {
-                (data: String, error: String?) -> Void in
-                if (error != nil) {
-                    callback(Dictionary<String, AnyObject>(), error)
-                } else {
-                    var jsonObj = self.JSONParseDict(data)
-                    callback(jsonObj, nil)
-                }
-            }
-    }
-
- */
-    
-
-
-
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
