9d6d3ac41bf13398eed0cccf59ebb6aa202d06ee
Alexander Wagner <awagner@bib.uni-mannheim.de>
2015-02-18 09:50:59 +0000

    JSON Test with iTunes Items, Successful Updating of TableView and Showing Images each row
    Signed-off-by: Alexander Wagner <alexander.wagner@bib.uni-mannheim.de>

--- UBMannheimApp/SeatsTableViewController.swift
+++ UBMannheimApp/SeatsTableViewController.swift
@@ -13,14 +13,93 @@ import Foundation
 
 class SeatsTableViewController: UITableViewController {
 
-    var items = ["A", "B"]
+    var items: AnyObject = []
     // var url: NSURL = NSURL()
 
+    var data: NSMutableData = NSMutableData()
+    
+    func searchItunesFor(searchTerm: String) {
+        
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        var itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        var escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        var urlPath = "https://itunes.apple.com/search?term="+escapedSearchTerm!+"&media=software"
+        var url: NSURL = NSURL(string: urlPath)!
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
+        
+        println("Search iTunes API at URL \(url)")
+        
+        connection.start()
+    }
+    
+    func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
+        // Recieved a new request, clear out the data object
+        self.data = NSMutableData()
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!) {
+        // Append the recieved chunk of data to our data object
+        self.data.appendData(data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        // Request complete, self.data should now hold the resulting info
+        // Convert the retrieved data in to an object through JSON deserialization
+        var err: NSError
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        
+        if jsonResult.count>0 /*&& jsonResult["results"].count>0*/ {
+            var results: NSArray = jsonResult["results"] as NSArray
+            self.items = results
+            self.tableView.reloadData()
+            
+        }
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "cell")
+        
+        var rowData: NSDictionary = self.items[indexPath.row] as NSDictionary
+        
+        cell.textLabel?.text = rowData["trackName"] as String
+        
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        var urlString: NSString = rowData["artworkUrl60"] as NSString
+        var imgURL: NSURL = NSURL(string: urlString)!
+        
+        // Download an NSData representation of the image at the URL
+        var imgData: NSData = NSData(contentsOfURL: imgURL)!
+        cell.imageView?.image = UIImage(data: imgData)
+        
+        // Get the formatted price string for display in the subtitle
+        var formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        
+        cell.detailTextLabel?.text = formattedPrice
+        
+        return cell
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        HTTPGetJSON("http://www.bib.uni-mannheim.de/bereichsauslastung/index.php?json") {
+        searchItunesFor("JQ Software")
+        
+        
+        
+        // let json = JSON(data: dataFromNetworking)
+        // http://www.bib.uni-mannheim.de/bereichsauslastung/index.php?json
+        
+        
+        /*HTTPGetJSON("http://www.bib.uni-mannheim.de/bereichsauslastung/index.php?json") {
             (data: Dictionary<String, AnyObject>, error: String?) -> Void in
             
             if (error != nil) {
@@ -62,13 +141,14 @@ class SeatsTableViewController: UITableViewController {
                     var n: NSString = name as NSString!
                     println(n)
                     
+                    self.items.append("C")
                 
                 }
    
             } // else
   
-        }
-            
+        }*/
+        
         // println("something")
         
         // Cell height.
@@ -81,16 +161,14 @@ class SeatsTableViewController: UITableViewController {
         self.tableView.reloadData()
         
     }
-    
+
+/*
+
     // func loadJSON(data: NSURL) {
     func loadJSON() {
         
         let urlAsString = "http://www.bib.uni-mannheim.de/bereichsauslastung/index.php?json"
         let url = "http://gdata.youtube.com/feeds/api/standardfeeds/most_popular?v=2&alt=json"
-
-        self.items.append("E")
-        
-        self.items.append("F")
         
         tableView.reloadData()
     }
@@ -175,7 +253,7 @@ class SeatsTableViewController: UITableViewController {
             }
     }
 
-    
+ */
     
 
 
