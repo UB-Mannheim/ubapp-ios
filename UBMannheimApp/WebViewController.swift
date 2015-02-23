6278fc90e6de6ff54e1a679769c2caecece23932
Alexander Wagner <awagner@bib.uni-mannheim.de>
2015-02-23 10:33:25 +0000

    Added Circular "ProgressBar" in NavigationBar of Webview
    Signed-off-by: Alexander Wagner <alexander.wagner@bib.uni-mannheim.de>

--- UBMannheimApp/WebViewController.swift
+++ UBMannheimApp/WebViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
     
@@ -18,11 +18,20 @@ class WebViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        /*
+            let requestURL = NSURL(string: website)
+            let request = NSURLRequest(URL: requestURL!)
+            webView.loadRequest(request)
+        */
         
-        let requestURL = NSURL(string: website)
-        let request = NSURLRequest(URL: requestURL!)
         
-        webView.loadRequest(request)
+        // check if necessary and why
+        // let webView:UIWebView = UIWebView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height))
+        webView.loadRequest(NSURLRequest(URL: NSURL(string: self.website)!))
+        webView.delegate = self
+        // check if necessary and why
+        // self.view.addSubview(webView)
         
     }
 
@@ -30,7 +39,70 @@ class WebViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    /*
+        // // // // // // // // // // // // // // // // // // // // // // // // //
+    
+        Circular ProgressBar (Upper right in NavigationBar)
+    */
+    
+    // show circular progress activity in upper navigation bar
+    // -start()
+    //  call: self.webViewDidStartLoad(webView)
+    
+    func webViewDidStartLoad(webView: UIWebView) { //start
+        var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .White)
+        var activityBarButtonItem = UIBarButtonItem(customView: activityIndicator)
+        navigationItem.rightBarButtonItem = activityBarButtonItem
+        activityIndicator.startAnimating()
+    }
+    
+    // show circular progress activity in upper navigation bar
+    // -stop()
+    //  call: self.webViewDidFinishLoad(webView)
+    
+    func webViewDidFinishLoad(webView: UIWebView) { //stop
+        navigationItem.rightBarButtonItem = nil
+    }
 
+    
+    
+    /*
+        // // // // // // // // // // // // // // // // // // // // // // // // //
+    
+        UIWebView Delegate Functions, requirement: extend class with <UIWebViewDelegate>
+    */
+    
+    // webview error
+    
+    func webView(webView: UIWebView!, didFailLoadWithError error: NSError!) {
+        print("Webview fail with error \(error)");
+    }
+    
+    // webview request
+    
+    func webView(webView: UIWebView!, shouldStartLoadWithRequest request: NSURLRequest!, navigationType: UIWebViewNavigationType) -> Bool {
+    return true;
+    }
+    
+    // webview start loading
+    
+    func webViewDidStartLoad(webView: UIWebView!) {
+        // print("Webview started Loading")
+        
+        webViewDidStartLoad(self.webView)
+    }
+    
+    // webview finished loading
+    
+    func webViewDidFinishLoad(webView: UIWebView!) {
+        // print("Webview did finish load")
+        
+        webViewDidFinishLoad(self.webView)
+        
+    }
+    
 
 }
 
