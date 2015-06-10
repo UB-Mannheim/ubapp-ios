d301b1c923a4b051f8c904221e94babbd456c0d2
Alexander Wagner <awagner@bib.uni-mannheim.de>
2015-06-10 14:38:21 +0000

    User Interface: Changed Startmenu, round corners in sublabels, 2do: positioning in relation to screen size
    Signed-off: Alexander Wagner <alexander.wagner@bib.uni-mannheim.de>

--- UBMannheimApp/MyCollectionViewCell.swift
+++ UBMannheimApp/MyCollectionViewCell.swift
@@ -15,6 +15,11 @@ class MyCollectionViewCell: UICollectionViewCell {
     
     var textLabel: UILabel!
     var imageView: UIImageView!
+    var uiView: UIView!
+    
+    var outerUIView: UIView!
+    var innerUIView: UIView!
+    var textLabel2: UILabel!
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -22,23 +27,58 @@ class MyCollectionViewCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         imageView = UIImageView(frame: CGRect(x: 0, y: 12, width: frame.size.width, height: frame.size.height*2/3))
         imageView.contentMode = UIViewContentMode.ScaleAspectFit
-        contentView.addSubview(imageView)
+    // contentView.addSubview(imageView)
         
-        let textFrame = CGRect(x: 0, y: frame.size.height/1.5, width: frame.size.width, height: frame.size.height/3)
+        
+        // uiView = UIView(frame: CGRect(x: 0, y: frame.size.height/1.5, width: frame.size.width, height: frame.size.height/3))
+    // uiView.layer.cornerRadius = 10.0
+    // contentView.addSubview(uiView)
+        // textLabel.backgroundColor = UIColor.redColor()
+
+        // rounded corners
+        // http://stackoverflow.com/questions/25591389/uiview-with-shadow-rounded-corners-and-custom-drawrect
+        
+        // let textFrame = CGRect(x: 0, y: frame.size.height/1.5, width: frame.size.width, height: frame.size.height/3)
+        let textFrame = CGRect(x: 0, y: frame.size.height/1.4, width: frame.size.width, height: frame.size.height/5)
         textLabel = UILabel(frame: textFrame)
+        
         // textLabel.font = UIFont.systemFontOfSize(UIFont.smallSystemFontSize())
         textLabel.font = UIFont.systemFontOfSize(frame.size.height/10)
-        textLabel.textColor = UIColor.whiteColor()
+        textLabel.textColor = UIColor.grayColor()
         textLabel.textAlignment = .Center
-        contentView.addSubview(textLabel)
+        
+    // contentView.addSubview(textLabel)
+        
+        outerUIView = UIView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height*2/3))
+        // imageView s.o.
+        innerUIView = UIView(frame: CGRect(x: 0, y: frame.size.height/1.4, width: frame.size.width, height: frame.size.height/5))
+        innerUIView.backgroundColor = UIColor.whiteColor()
+        innerUIView.layer.cornerRadius = 10.0
+        innerUIView.layer.opacity = 0.8
+        // textLabel s.o.
+        
+        textLabel2 = UILabel(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height/5))
+        textLabel2.font = UIFont.systemFontOfSize(frame.size.height/10)
+        textLabel2.textColor = UIColor.grayColor()
+        textLabel2.textAlignment = .Center
+        
+        innerUIView.addSubview(textLabel2) // not added - but why?
+        
+        outerUIView.addSubview(imageView)
+        outerUIView.addSubview(innerUIView)
+        
+        contentView.addSubview(outerUIView)
+        contentView.addSubview(innerUIView)
+        
     }
     
     func setMenuItem(item: MenuItem) {
         // self.backgroundColor = UIColor.blackColor()
         self.textLabel?.text = item.itemTitle
+        self.textLabel2?.text = item.itemTitle
         self.imageView?.image = UIImage(named: item.itemImage)
     }
 }
